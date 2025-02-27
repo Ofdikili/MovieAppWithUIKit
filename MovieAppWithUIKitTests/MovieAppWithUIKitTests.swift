b333e47cfd9d3c867ed9c5cdca96575dc74599d5
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-27 14:06:07 +0300

    Initial Commit

---
+++ MovieAppWithUIKitTests/MovieAppWithUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppWithUIKitTests.swift
+//  MovieAppWithUIKitTests
+//
+//  Created by Ömer Faruk Dikili on 27.02.2025.
+//
+
+import Testing
+@testable import MovieAppWithUIKit
+
+struct MovieAppWithUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
